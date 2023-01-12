0224d30d6bf7fca83e7e41fa264fc8eea1216d8b
PavelETU <pavelsuvit@gmail.com>
2023-01-12 16:52:58 +0000

    Add initial Android project structure (Compose Material 3 template)

---
+++ app/src/main/java/com/abakan/electronics/mycountries/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.abakan.electronics.mycountries
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.abakan.electronics.mycountries.ui.theme.MyCountriesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyCountriesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyCountriesTheme {
+        Greeting("Android")
+    }
+}
